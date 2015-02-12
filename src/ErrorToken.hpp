105a571a781e4cd78f7b6420236f88b111f0f481
Jason Rice <ricejasonf@gmail.com>
2015-02-11 19:34:11 -0800

    ErrorToken sounds better

---
+++ src/ErrorToken.hpp
@@ -0,0 +1,16 @@
+#ifndef NBDL_ERROR_TOKEN_HPP
+#define NBDL_ERROR_TOKEN_HPP
+
+namespace nbdl {
+
+	enum struct ErrorToken
+	{
+		Invalid,
+		Required,
+		OutOfRange,
+		MatchFail
+	}
+
+}//nbdl
+
+#endif
