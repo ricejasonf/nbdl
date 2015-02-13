41682bc7ff833e141d5f59199f0648beb53fcf04
Jason Rice <ricejasonf@gmail.com>
2015-02-12 21:05:18 -0800

    ErrorToken to string conversion

--- src/ErrorToken.hpp
+++ src/ErrorToken.hpp
@@ -3,16 +3,47 @@
 
 namespace nbdl {
 
-	enum struct ErrorToken
-	{
-		Invalid,
-		Required,
-		TooLong,
-		TooShort,
-		AboveMax,
-		BelowMin,
-		MatchFail
-	}
+//todo make macro to build this
+enum struct ErrorToken
+{
+	Invalid,
+	Required,
+	TooLong,
+	TooShort,
+	AboveMax,
+	BelowMin,
+	MatchFail,
+	Custom1 = 1000,
+	Custom2,
+	Custom3,
+	Custom4,
+	Custom5,
+	Custom6,
+	Custom7,
+	Custom8,
+	Custom9
+}
+
+static constexpr const char *toString(ErrorToken t)
+{
+	return	t == ErrorToken::Invalid	? "Invalid" 	:
+			t == ErrorToken::Required	? "Required" 	:
+			t == ErrorToken::TooLong	? "TooLong" 	:
+			t == ErrorToken::TooShort	? "TooShort" 	:
+			t == ErrorToken::AboveMax	? "AboveMax" 	:
+			t == ErrorToken::BelowMin	? "BelowMin" 	:
+			t == ErrorToken::MatchFail	? "MatchFail" 	:
+			t == ErrorToken::Custom1	? "Custom1" 	:
+			t == ErrorToken::Custom2	? "Custom2" 	:
+			t == ErrorToken::Custom3	? "Custom3" 	:
+			t == ErrorToken::Custom4	? "Custom4" 	:
+			t == ErrorToken::Custom5	? "Custom5" 	:
+			t == ErrorToken::Custom6	? "Custom6" 	:
+			t == ErrorToken::Custom7	? "Custom7" 	:
+			t == ErrorToken::Custom8	? "Custom8" 	:
+			t == ErrorToken::Custom9	? "Custom9" 	:
+			"Invalid";
+}
 
 }//nbdl
 
