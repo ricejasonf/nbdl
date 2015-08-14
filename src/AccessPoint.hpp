e86e0563674d694a9d32d37ef922c3beb9f98575
Jason Rice <ricejasonf@gmail.com>
2015-08-14 11:45:08 -0700

    improved access point definitions

--- src/AccessPoint.hpp
+++ src/AccessPoint.hpp
@@ -1,6 +1,7 @@
 #ifndef NBDL_ACCESS_POINT_HPP
 #define NBDL_ACCESS_POINT_HPP
 
+#include<type_traits>
 #include<tuple>
 #include<boost/functional/hash.hpp>
 
@@ -48,17 +49,40 @@ struct PredAccessPointFn
 
 }//anon
 
+namespace details {
+
+template<typename AccessPointType, typename EntityType, int i, class = void>
+struct FindAccessPoint
+{
+	using Parent = typename AccessPointType::Parent;
+	static const int value = FindAccessPoint<Parent, EntityType, (i + 1)>::value;
+};
+template<typename AccessPointType, typename EntityType, int i>
+struct FindAccessPoint<AccessPointType, EntityType, i,
+	typename std::enable_if<
+		std::is_same<typename AccessPointType::Entity, EntityType>::value
+	>::type>
+{
+	static const int value = i;
+};
+
+}//details
+
 template<typename EntityType, typename KeyType, typename ParentAccessPoint = void>
 class AccessPoint
 {
 	public:
 
+	using Parent = ParentAccessPoint;
 	using ParentTuple = typename ParentAccessPoint::Tuple;
+	using ParentKey = typename ParentAccessPoint::Key;
 	using Tuple = decltype(std::tuple_cat(std::declval<std::tuple<KeyType>>(), std::declval<ParentTuple>()));
 	using Key = KeyType;
 	using Entity = EntityType;
 	using HashFn = HashAccessPointFn<AccessPoint>;
 	using PredFn = PredAccessPointFn<AccessPoint>;
+	template<typename E, int i = 0>
+	using FindAccessPoint = details::FindAccessPoint<AccessPoint, E, i>;
 
 	Tuple tuple;
 
@@ -76,9 +100,15 @@ class AccessPoint
 		tuple(t)
 	{}
 
+	template<typename... Args>
+	AccessPoint(Key k, ParentKey pk, Args... args) : 
+		tuple(std::make_tuple(k, pk, args...))
+	{}
+
+	template<typename E = EntityType>
 	KeyType getKey()
 	{
-		return std::get<0>(tuple);
+		return std::get<FindAccessPoint<E>::value>(tuple);
 	}
 
 	ParentAccessPoint getParent()
@@ -136,6 +166,18 @@ class AccessPoint<EntityType, KeyType, void>
 
 template<typename AccessPoint>
 struct AccessPointName {};
+
+template<typename... Args>
+struct CreateAccessPoint 
+{
+	using Type = void;
+};
+template<typename AccessPointType, typename... Args>
+struct CreateAccessPoint<AccessPointType, Args...>
+{
+	using Type = AccessPoint<typename AccessPointType::Entity, typename AccessPointType::Key, typename CreateAccessPoint<Args...>::Type>;
+};
+
 }//nbdl
 
 #endif
--- test/src/access_point.cpp
+++ test/src/access_point.cpp
@@ -11,6 +11,16 @@ struct MyEntity
 	int id;
 	int client_id;
 };
+struct MyEntityAttribute
+{
+	int id;
+	int my_entity_id;
+};
+struct AttributeStatus
+{
+	int id;
+	int attribute_id;
+};
 namespace nbdl {
 	NBDL_ENTITY(
 		Client,
@@ -19,6 +29,14 @@ namespace nbdl {
 		MyEntity,
 			id,
 			client_id );
+	NBDL_ENTITY(
+		MyEntityAttribute,
+			id,
+			my_entity_id );
+	NBDL_ENTITY(
+		AttributeStatus,
+			id,
+			attribute_id );
 }//nbdl
 
 TEST_CASE("Create a root level access point.", "[api]") 
@@ -34,6 +52,27 @@ TEST_CASE("Create a nested access point.", "[api]")
 	B b = B(25, A(1));
 	REQUIRE(b.getKey() == 25);
 	REQUIRE(b.getParent().getKey() == 1);
+}
+TEST_CASE("Create a deeply nested access point.", "[api]") 
+{
+	using A = typename nbdl::CreateAccessPoint<
+		nbdl::AccessPoint<Client, int>,
+		nbdl::AccessPoint<MyEntity, int>,
+		nbdl::AccessPoint<MyEntityAttribute, int>,
+		nbdl::AccessPoint<AttributeStatus, int>
+	>::Type;
+	//lexicographic orderings
+	A a(1, 2, 3, 4);
+	
+	REQUIRE(a.getKey<Client>() == 1);
+	REQUIRE(a.getKey<MyEntity>() == 2);
+	REQUIRE(a.getKey<MyEntityAttribute>() == 3);
+	REQUIRE(a.getKey<AttributeStatus>() == 4);
+
+	REQUIRE(a.getKey() == 1);
+	REQUIRE(a.getParent().getKey() == 2);
+	REQUIRE(a.getParent().getParent().getKey() == 3);
+	REQUIRE(a.getParent().getParent().getParent().getKey() == 4);
 }
 
 /* test takes a long time
