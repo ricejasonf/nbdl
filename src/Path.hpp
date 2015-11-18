6bba485ce32164e214710fb5ef754e0dc940bd99
Jason Rice <ricejasonf@gmail.com>
2015-11-18 15:53:39 -0800

    wip Path refactor

--- src/Path.hpp
+++ src/Path.hpp
@@ -20,198 +20,112 @@ namespace nbdl {
 
 namespace path_details {
 
-//todo break this out and give it a start and a stop
-template<int ...>
-struct NumberSequence {};
-template<int n, int... sequence>
-struct GenerateNumberSequence
+struct Bottom
 {
-	using Type = typename GenerateNumberSequence<n - 1, n - 1, sequence...>::Type;
-};
-template<int... sequence>
-struct GenerateNumberSequence<0, sequence...>
-{
-	using Type = NumberSequence<sequence...>;
-};
-
-//Hash Function
-template<typename PathType>
-struct HashPathFn
-{
-	using Tuple = typename PathType::Tuple;
-	using Hasher = typename PathType::template Hasher<0, Tuple>;
-
-	std::size_t operator() (const PathType& path) const
-	{
-		std::size_t seed;
-		Hasher::call(seed, path.tuple);
-		return seed;
-	}
-};
-
-template<typename PathType>
-struct PredPathFn
-{
-	bool operator() (const PathType& t1, const PathType& t2) const
-	{
-		return t1.tuple == t2.tuple;
-	}
-};
-
-template<typename PathType, typename EntityType, int i, class = void>
-struct FindPath
-{
-	using Parent = typename PathType::Parent;
-	static const int value = FindPath<Parent, EntityType, (i - 1)>::value;
-};
-template<typename PathType, typename EntityType, int i>
-struct FindPath<PathType, EntityType, i,
-	typename std::enable_if<
-		std::is_same<typename PathType::Entity, EntityType>::value
-	>::type>
-{
-	static const int value = i;
+  static constexpr auto entityTypes()
+  {
+    return hana::make_map();
+  }
+  using Storage = decltype(hana::make_tuple());
+  Bottom() = delete;
+  Bottom(const Bottom&) = delete;
 };
 
 }//path_details
 
-template<typename EntityType, typename KeyType, typename ParentPath = void>
+template<typename Entity_, typename Key, typename ParentPath = path_details::Bottom>
 class Path
 {
+  //uhh change this back
+  static auto entityTypes()
+  {
+    return hana::unpack(
+      hana::range_c<int, 0, sizeof...(ParentEntities) - 1>,
+      [](auto... i) {
+        constexpr auto parents = hana::tuple_t<ParentEntities...>;
+        return hana::make_map(hana::make_pair(
+          hana::at(parents, i), i)...);
+      });
+  }
+
+  template<typename T>
+  static constexpr auto entityTypeIndex(T type)
+  {
+    auto types = entityTypes();
+    return types[type];
+  }
+
+	using Storage = decltype(hana::append(
+    typename std::declval<typename ParentPath::Storage>(), std::declval<Key>()));
+
+	Storage storage;
+
 	public:
 
-	using Parent = ParentPath;
-	using ParentTuple = typename ParentPath::Tuple;
-	using ParentKey = typename ParentPath::Key;
-	using Tuple = decltype(std::tuple_cat(std::declval<ParentTuple>(), std::declval<std::tuple<KeyType>>()));
-	using Key = KeyType;
-	using Entity = EntityType;
-	using HashFn = path_details::HashPathFn<Path>;
-	using PredFn = path_details::PredPathFn<Path>;
-	template<typename E, int i = std::tuple_size<Tuple>::value - 1>
-	using FindPath = path_details::FindPath<Path, E, i>;
-
-	Tuple tuple;
-
-	private:
-
-	template<int... sequence>
-	ParentTuple makeParentTuple(path_details::NumberSequence<sequence...>) const
-	{
-		return std::make_tuple(std::get<sequence>(tuple)...);
-	}
-
-	public:
+	using Entity = Entity_;
 
 	Path(Key k, ParentPath p) : 
-		tuple(std::tuple_cat(p.tuple, std::make_tuple(k)))
-	{}
-
-	Path(Tuple t) :
-		tuple(t)
+    storage(hana::append(p.storage, hana::type_c<Entity_>, k))
 	{}
 
 	template<typename... Args>
 	Path(Args... args) :
-		tuple(std::make_tuple(args...))
+    storage(hana::make_tuple(args...))
 	{}
 
-	template<typename E = EntityType>
+	template<typename E = Entity_>
 	KeyType getKey() const
 	{
-		return std::get<FindPath<E>::value>(tuple);
+    static constexpr entity_type_index = hana::at_key(entityTypes(), hana::type_c<E>);
+		return hana::at(storage, entityTypeIndex());
 	}
 
-	ParentPath getParent() const
+	ParentPath parent() const
 	{
-		return ParentPath(makeParentTuple(typename path_details::GenerateNumberSequence<(std::tuple_size<Tuple>::value - 1)>::Type()));
+    return hana::drop_back(storage, hana::size_c<1>);
 	}
 
-	template<int i, typename T>
-	struct Hasher
-	{
-		using ParentHasher = typename ParentPath::template Hasher<(i + 1), T>;
-		static void call(std::size_t& seed, const T& tuple)
-		{
-			nbdl::details::HashCombine(seed, std::get<i>(tuple));
-			ParentHasher::call(seed, tuple);
-		}
-	};
+  struct HashFn
+  {
+    auto operator()(std::size_t& seed, const Storage& storage)
+    {
+      for_each(storage, [&](const auto& x) {
+          nbdl::details::HashCombine(seed, x); 
+        });
+    }
+  };
+
+  struct PredFn
+  {
+    bool operator() (const PathType& t1, const PathType& t2) const
+    {
+      return t1.storage == t2.storage;
+    }
+  };
+
 };
 
-template<typename EntityType, typename KeyType>
-class Path<EntityType, KeyType, void>
-{
-	public:
-
-	using Tuple = std::tuple<KeyType>;
-	using Key = KeyType;
-	using Entity = EntityType;
-	using HashFn = path_details::HashPathFn<Path>;
-	using PredFn = path_details::PredPathFn<Path>;
-
-	Tuple tuple;
-
-	Path(Key k) : 
-		tuple(std::make_tuple(k)) 
-	{}
-
-	Path(Tuple t) :
-		tuple(t)
-	{}
-
-	KeyType getKey()
-	{
-		return std::get<0>(tuple);
-	}
-
-	template<int i, typename T>
-	struct Hasher
-	{
-		static void call(std::size_t& seed, const T& tuple)
-		{
-			nbdl::details::HashCombine(seed, std::get<i>(tuple));
-		}
-	};
-};
-
-template<typename Path>
-struct PathName {};
-
-template<typename PathType, class = void>
-struct CreatePathNode
-{
-	using Type = PathType;
-};
-template<typename EntityType>
-struct CreatePathNode<EntityType,
-	typename std::enable_if<IsEntity<EntityType>::value>::type>
-{
-	using Type = Path<EntityType, typename PrimaryKey<EntityType>::MemberType>;
-};
-
-template<typename Tuple>
-struct CreatePathFromReversedTuple;
-
-template<typename... Args>
-struct CreatePathFromReversedTuple<std::tuple<Args...>>
-{
-	using Type = void;
-};
-
-template<typename PathType, typename... Args>
-struct CreatePathFromReversedTuple<std::tuple<PathType, Args...>>
-{
-	using PathNode = typename CreatePathNode<PathType>::Type;
-	using Type = Path<typename PathNode::Entity, typename PathNode::Key, typename CreatePathFromReversedTuple<std::tuple<Args...>>::Type>;
-};
-
+//todo makePath function
 template<typename... Args>
 struct CreatePath
 {
 	using ReversedTuple = typename mpl::Reverse<std::tuple<Args...>>::Type;
 	using Type = typename CreatePathFromReversedTuple<ReversedTuple>::Type;	
 };
+
+template<typename... Pairs>
+constexpr auto makePathType(Pairs... pairs)
+{
+  using Spec = decltype(hana::make_tuple(pairs...));
+  return hana::type_c<Path<Spec>>;
+}
+
+//This assumes all entities use int for their primary keys.
+template<typename... EntityTypes>
+constexpr auto makePathTypeFromEntities(EntityTypes... entity_types)
+{
+  return makePathType(entity_types, hana::type_c<int>)...;
+}
 
 }//nbdl
 
