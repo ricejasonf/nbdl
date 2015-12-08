c9f05d22c039accc64092b665b8ac981343fdcff
Jason Rice <ricejasonf@gmail.com>
2015-12-07 22:50:03 -0800

    general make function

---
+++ src/Make.hpp
@@ -0,0 +1,24 @@
+//
+// Copyright Jason Rice 2015
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#ifndef NBDL_MAKE_HPP
+#define NBDL_MAKE_HPP
+
+#include<type_traits>
+
+//general high order make function
+template<template<class...> class T>
+struct Make
+{
+  template<typename... X>
+  constexpr auto operator()(X... x)
+  {
+    return T<std::decay_t<decltype(x)>...>
+      (std::forward<decltype(x)>(x)...);
+  }
+};
+
+#endif
