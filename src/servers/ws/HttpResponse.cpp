d375d0e738a313d5788f9b45ea0a06fd311614fc
Jason Rice <ricejasonf@gmail.com>
2016-11-12 16:14:30 -0800

    Removed openssl dependency

--- src/servers/ws/HttpResponse.cpp
+++ src/servers/ws/HttpResponse.cpp
@@ -1,22 +1,32 @@
-#include<cstring>
-#include<openssl/sha.h>
+//
+// Copyright Jason Rice 2014-2016
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <nbdl/detail/beast_sha1.hpp>
+#include <nbdl/util/base64_encode.hpp>
+
+#include <array>
 #include "HttpResponse.hpp"
-#include "../../openssl/Base64Encode.hpp"
 
 namespace ws = nbdl::servers::ws;
-using Base64Encode = nbdl::openssl::Base64Encode;
+namespace sha1 = nbdl::detail::beast_sha1;
 
-void ws::http_response::generate_accept_token(const std::string& key)
+void ws::http_response::generate_accept_token(std::string const& key)
 {
-	const char* guid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-	constexpr int guid_len = 36;
-	SHA_CTX ctx;
-	unsigned char digest[SHA_DIGEST_LENGTH];
+  using nbdl::util::base64_encode;
 
-	SHA1_Init(&ctx);
-	SHA1_Update(&ctx, key.c_str(), key.size());
-	SHA1_Update(&ctx, guid, guid_len);
-	SHA1_Final(digest, &ctx);
-	Base64Encode encoding(reinterpret_cast<const char*>(digest), SHA_DIGEST_LENGTH);
-	encoding.appendTo(response);
+	const char* guid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+	constexpr int guid_length = 36;
+  std::array<unsigned char, 20> digest{};
+  sha1::sha1_context ctx{};
+
+  sha1::init(ctx);
+  sha1::update(ctx, key.data(), key.length());
+  sha1::update(ctx, guid, guid_length);
+  sha1::finish(ctx, digest.data());
+
+  response.append(base64_encode(digest));
 }
