feb5c6cd869ca1ac9ed9f94cd6d19002db410a8a
Jason Rice <ricejasonf@gmail.com>
2017-05-13 21:33:04 -0700

    [run_sync][match] Adds tests

---
+++ test/nbdl/core/match.cpp
@@ -0,0 +1,140 @@
+//
+// Copyright Jason Rice 2017
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <nbdl/entity.hpp>
+#include <nbdl/match.hpp>
+#include <nbdl/variant.hpp>
+
+#include <boost/hana/equal.hpp>
+#include <boost/hana/map.hpp>
+#include <boost/hana/string.hpp>
+#include <boost/hana/tuple.hpp>
+#include <catch.hpp>
+#include <string>
+
+#include <iostream>
+
+namespace hana = boost::hana;
+using namespace hana::literals;
+
+namespace
+{
+  struct name_first_t { std::string value; };
+  struct name_last_t { std::string value; };
+
+  struct person_t
+  {
+    std::string name_first;
+    std::string name_last;
+  };
+}
+
+namespace nbdl
+{
+  NBDL_ENTITY(
+    person_t
+    , name_first
+    , name_last
+  );
+}
+
+TEST_CASE("Match on values in a hana::map.", "[match][Store]")
+{
+  std::string result("FAIL!");
+
+  auto store = hana::make_map(
+    hana::make_pair("key_1"_s, std::string("Hello, World!"))
+  , hana::make_pair("key_2"_s, 42)
+  );
+
+  nbdl::match(
+    store
+  , "key_1"_s
+  , [&](std::string const& x) { result = x; }
+  );
+
+  // Hello, World!
+
+  CHECK(result == std::string{"Hello, World!"});
+}
+
+TEST_CASE("Match on values in a nbdl::Entity.", "[match][Store]")
+{
+  std::string result("FAIL!");
+
+  auto store = person_t{"John", "Smith"};
+
+  nbdl::match(
+    store
+  , "name_first"_s
+  , [&](std::string const& x) { result = x; }
+  );
+
+  // John
+
+  CHECK(result == std::string{"John"});
+}
+
+TEST_CASE("Match on values in a hana::tuple.", "[match][Store]")
+{
+  std::string result("FAIL!");
+
+  auto name_first = hana::type_c<name_first_t>;
+
+  auto store = hana::make_tuple(name_first_t{"John"}, name_last_t{"Smith"});
+
+  nbdl::match(
+    store
+  , name_first
+  , [&](name_first_t const& x) { result = x.value; }
+  );
+
+  // John
+
+  CHECK(result == std::string{"John"});
+}
+
+TEST_CASE("Match on values in a variant.", "[match][Store]")
+{
+  std::string result("FAIL!");
+
+  nbdl::optional<std::string> store = std::string("Hello, maybe!");
+
+  nbdl::match(
+    store
+  , [&](auto const& x)
+    {
+      if constexpr(hana::is_a<std::string, decltype(x)>)
+        result = x;
+    }
+  );
+
+  // Hello, maybe!
+
+  CHECK(result == std::string{"Hello, maybe!"});
+}
+
+TEST_CASE("Match on values nested in a variant.", "[match][Store]")
+{
+  std::string result("FAIL!");
+
+  nbdl::optional<person_t> store = person_t{"John", "Smith"};
+
+  nbdl::match(
+    store
+  , "name_first"_s
+  , [&](auto const& x)
+    {
+      if constexpr(hana::is_a<std::string, decltype(x)>)
+        result = x;
+    }
+  );
+
+  // John
+
+  CHECK(result == std::string{"John"});
+}
---
+++ test/nbdl/core/run_sync.cpp
@@ -0,0 +1,132 @@
+//
+// Copyright Jason Rice 2017
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <nbdl/catch.hpp>
+#include <nbdl/entity.hpp>
+#include <nbdl/match.hpp>
+#include <nbdl/path_promise.hpp>
+#include <nbdl/promise.hpp>
+#include <nbdl/run_sync.hpp>
+#include <nbdl/tap.hpp>
+#include <nbdl/variant.hpp>
+
+#include <algorithm>
+#include <boost/hana/equal.hpp>
+#include <boost/hana/map.hpp>
+#include <boost/hana/string.hpp>
+#include <boost/hana/tuple.hpp>
+#include <catch.hpp>
+#include <string>
+
+#include <iostream>
+
+namespace hana = boost::hana;
+using namespace hana::literals;
+
+namespace
+{
+  struct username_t { std::string value; };
+  struct name_first_t { std::string value; };
+  struct name_last_t { std::string value; };
+
+  struct person_t
+  {
+    hana::tuple<name_first_t, name_last_t> name;
+  };
+
+  struct account_t
+  {
+    username_t username;
+    nbdl::optional<person_t> person;
+  };
+
+  // used for example
+  template <typename T>
+  auto require_type = nbdl::promise([](auto& resolver, auto const& value)
+  {
+    if constexpr(decltype(hana::typeid_(value) == hana::type_c<T>){})
+      resolver.resolve(value);
+    else
+      resolver.reject(value);
+  });
+}
+
+namespace nbdl
+{
+  NBDL_ENTITY(
+    person_t
+    , name
+  );
+
+  NBDL_ENTITY(
+    account_t
+    , username
+    , person
+  );
+} // nbdl
+
+TEST_CASE("Promise to a nested value in a store", "[run_sync][params_promise]")
+{
+  using namespace nbdl::ui_spec;
+
+  std::string result{"FAIL!"};
+
+  account_t account{
+    username_t{"@JasonRice_"}
+  , person_t{hana::make_tuple(name_first_t{"Skippy"}, name_last_t{"McGee"})}
+  };
+
+  auto value_of = [](auto const& x) { return x.value; };
+  auto to_upper = [](std::string str) { for (auto & c: str) c = ::toupper(c); return str; };
+
+  nbdl::run_sync(
+    nbdl::pipe(
+      nbdl::path_promise(get("person"_s, "name"_s, hana::type_c<name_last_t>))
+    , require_type<name_last_t>
+    , value_of
+    , to_upper
+    , nbdl::tap([&](std::string const& str) { result = str; })
+    , nbdl::catch_([](auto&&) { })
+    )
+  , account
+  );
+
+  // MCGEE
+
+  CHECK(result == std::string{"MCGEE"});
+}
+
+TEST_CASE("Reject promise to a nested value in a store", "[run_sync][path_promise]")
+{
+  using namespace nbdl::ui_spec;
+
+  std::string result{"FAIL!"};
+
+  account_t account{
+    username_t{"@JasonRice_"}
+  , nbdl::nothing{}
+  };
+
+  auto value_of = [](auto const& x) { return x.value; };
+  auto to_upper = [](std::string str) { for (auto & c: str) c = ::toupper(c); return str; };
+
+  nbdl::run_sync(
+    nbdl::pipe(
+      nbdl::path_promise(get("person"_s, "name"_s, hana::type_c<name_last_t>))
+    , require_type<name_last_t>
+    , value_of
+    , to_upper
+    , nbdl::tap([](std::string const&) { })
+    , nbdl::catch_([&](auto&&) { result = "something else"; })
+    )
+  , account
+  );
+
+  // something else
+
+  CHECK(result == std::string{"something else"});
+}
