afb09d6197a01b2c543c7f9637d519ce2755e497
Jason Rice <ricejasonf@gmail.com>
2020-06-09 19:05:56 -0700

    Fixes mpdef::to_constant heavy_macro issues

--- include/mpdef/utility.hpp
+++ include/mpdef/utility.hpp
@@ -12,20 +12,30 @@
 #include <type_traits>
 #include <utility>
 
-namespace mpdef
-{
+namespace mpdef {
   namespace hana = boost::hana;
 
+  // works around clang bug reported on llvm bugs #20033
+  heavy_macro convert_string(s, ...i) = [] {
+    constexpr auto len = sizeof(s) - 1;
+    if constexpr(len > sizeof...(i))
+      return convert_string(s, i..., sizeof...(i));
+    else
+      return hana::string<s[i]...>{};
+  }();
+
   // idempotently converts to a hana Constant or string
-  heavy_macro to_constant(x) = [] {
-    constexpr auto t = hana::traits::decay(hana::typeid_(x));
-    using T = typename decltype(t)::type;
+  heavy_macro to_constant(x) = [](auto inst) {
+    using T = std::decay_t<decltype(inst)>;
 
     if constexpr (std::is_integral_v<T>)
-      return hana::integral_constant<T, x>{};
-    else if constexpr (std::is_same_v<char const*, T>)
-      return BOOST_HANA_STRING(x);
-    else return x;
-  }();
+      return std::integral_constant<T, x>{};
+    else if constexpr (std::is_same_v<const char*, T>)
+      return convert_string(x);
+    else return inst;
+  }(x);
+
+  static_assert(convert_string("foo") == hana::string<'f', 'o', 'o'>{});
 }
+
 #endif
